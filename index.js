91e99773d40b9247d908923cda054c762ebd600d
Jagdish-2023 <jagdishjpradhan@gmail.com>
2024-10-07 16:58:30 +0530

    feat: API routes to update Recipe  by Id, Title

--- index.js
+++ index.js
@@ -148,6 +148,64 @@ const getRecipesByEasyLevel = async (difficultyLevel) => {
   }
 };
 
+//API to update Difficulty level by Id(66fe4684424c844028dfeed3)
+app.post("/recipes/:recipeId", async (req, res) => {
+  try {
+    const toUpdateData = req.body;
+    const updatedRecipe = await updateRecipeDifficultyById(
+      req.params.recipeId,
+      toUpdateData
+    );
+    if (updatedRecipe) {
+      res
+        .status(200)
+        .json({ message: "Recipe updated successfully", updatedRecipe });
+    } else {
+      res.status(404).json({ error: "Recipe not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to updated the Recipe." });
+  }
+});
+
+const updateRecipeDifficultyById = async (recipeId, toUpdateData) => {
+  try {
+    return await Recipe.findByIdAndUpdate(recipeId, toUpdateData, {
+      new: true,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+//API to update fields data(cookTime, prepTime) by the Title
+app.post("/recipes/recipe/:recipeTitle", async (req, res) => {
+  try {
+    const dataToUpdate = req.body;
+    const recipeTitle = req.params.recipeTitle;
+    const updatedRecipe = await updateRecipeByTitle(recipeTitle, dataToUpdate);
+    if (updatedRecipe) {
+      res
+        .status(200)
+        .json({ message: "Recipe updated successfully", updatedRecipe });
+    } else {
+      res.status(404).json({ error: "Recipe not found." });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to update Recipe" });
+  }
+});
+
+const updateRecipeByTitle = async (recipeTitle, dataToUpdate) => {
+  try {
+    return await Recipe.findOneAndUpdate({ title: recipeTitle }, dataToUpdate, {
+      new: true,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
