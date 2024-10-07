7a6f58aa65844fa5803c33af9b5d03156a73311c
Jagdish-2023 <jagdishjpradhan@gmail.com>
2024-10-07 14:28:44 +0530

    feat: adding API routes to get Recipes by Tile, Author and Difficulty

--- index.js
+++ index.js
@@ -58,6 +58,96 @@ const addRecipeData = async (recipeData) => {
   }
 };
 
+// API to get all the recipes
+app.get("/recipes", async (req, res) => {
+  try {
+    const getRecipes = await getAllRecipes();
+    if (getRecipes.length > 0) {
+      res.status(200).json(getRecipes);
+    } else {
+      res.status(404).json({ error: "No recipes found in Database." });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to get recipes." });
+  }
+});
+
+const getAllRecipes = async () => {
+  try {
+    return await Recipe.find();
+  } catch (error) {
+    throw error;
+  }
+};
+
+//API to get Recipe by its Title
+app.get("/recipes/:recipeTitle", async (req, res) => {
+  try {
+    const recipe = await getRecipeByTitle(req.params.recipeTitle);
+    if (recipe) {
+      res.status(200).json(recipe);
+    } else {
+      res.status(404).json({ error: "Recipe not found." });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to get recipe." });
+    console.log(error);
+  }
+});
+
+const getRecipeByTitle = async (recipeTitle) => {
+  try {
+    return await Recipe.findOne({ title: recipeTitle });
+  } catch (error) {
+    throw error;
+  }
+};
+
+//API to get all Recipes by Author
+app.get("/recipes/author/:recipeAuthor", async (req, res) => {
+  try {
+    const recipes = await getRecipesByAuthor(req.params.recipeAuthor);
+    if (recipes.length > 0) {
+      res.status(200).json(recipes);
+    } else {
+      res.status(404).json({ error: "Recipes not found." });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch Recipes." });
+    console.log(error);
+  }
+});
+
+const getRecipesByAuthor = async (recipeAuthor) => {
+  try {
+    return await Recipe.find({ author: recipeAuthor });
+  } catch (error) {
+    throw error;
+  }
+};
+
+//API to get all Recipes with EASY difficulty level
+app.get("/recipes/difficulty/:difficultLevel", async (req, res) => {
+  try {
+    const recipes = await getRecipesByEasyLevel(req.params.difficultLevel);
+    if (recipes.length > 0) {
+      res.status(200).json(recipes);
+    } else {
+      res.status(404).json({ error: "Recipes not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch Recipes" });
+  }
+});
+
+const getRecipesByEasyLevel = async (difficultyLevel) => {
+  try {
+    return await Recipe.find({ difficulty: difficultyLevel });
+  } catch (error) {
+    throw error;
+  }
+};
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
